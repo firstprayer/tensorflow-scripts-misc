bf8c45baa54275a98b6796198ae4ac0f8fb2adfa
Taiyuan Zhang <taiyuanz@fb.com>
2017-01-30 21:30:46 -0800

    fix loss function

--- neuralstyle/synthesis.py
+++ neuralstyle/synthesis.py
@@ -9,10 +9,11 @@ import time
 import os
 import scipy
 from scipy import misc
-from vgg import vgg19
+# from vgg import vgg19
 
 FLAGS = None
 IMAGENET_MEAN = 117.0
+STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
 
 
 def load_vgg19():
@@ -22,15 +23,32 @@ def load_vgg19():
           name='input', \
           shape=[1, FLAGS.img_height, FLAGS.img_width, 3], \
           initializer=tf.random_normal_initializer())
-  model.build(t_input - IMAGENET_MEAN)
+  model.build(t_input / 255.0)
+  return t_input
+
+
+def load_vgg19_v2():
+  from neural_style import vgg
+
+  # model = vgg19.Vgg19(FLAGS.model_path)
+
+  t_input = tf.get_variable(
+          name='input', \
+          shape=[1, FLAGS.img_height, FLAGS.img_width, 3], \
+          initializer=tf.random_normal_initializer())
+
+  red, green, blue = tf.split(3, 3, t_input)
+  print(red.get_shape().as_list(), green.get_shape().as_list(), blue.get_shape().as_list())
+  normalized_input = tf.concat(3, [red - 123.68, green - 116.779, blue - 103.939])
+  vgg.net('imagenet-vgg-verydeep-19.mat', normalized_input)
   return t_input
 
 
 def print_graph_node_names(graph):
   '''For printing out graph structure'''
   for node in graph.as_graph_def().node:
-    if node.op != 'Const':
-      print(node.name, node.op) 
+    # if node.op == 'Const':
+    print(node.name, node.op) 
 
 
 def T(graph, layer):
@@ -47,61 +65,95 @@ def imread(path):
   return img
 
 
-def main():
-  graph = tf.Graph()
-  with graph.as_default():
+def get_activation():
+  with tf.Graph().as_default() as graph:
     session = tf.InteractiveSession(graph=graph)
-    # Load pretrained model
-    X = load_vgg19()
-
-    content_image = imread(FLAGS.content_image)
-    print(content_image.flatten()[: 20])
+    X = load_vgg19_v2()
 
     style_image = imread(FLAGS.style_image)
-    print(style_image.flatten()[: 20])
     
     style_layers = [
       # VGG19 layers
-      'conv1_1/Relu',
-      'conv2_1/Relu',
-      'conv3_1/Relu',
-      'conv4_1/Relu',
-      'conv5_1/Relu',
+      'Relu',
+      'Relu_2',
+      'Relu_4',
+      'Relu_8',
+      'Relu_12',
     ]
     layer_style_loss_list = []
     session.run(tf.global_variables_initializer())
     session.run(X.assign(style_image)) 
     for layer_name in style_layers:
       t_layer = T(graph, layer_name)
-      t_layer_shape = t_layer.get_shape().as_list()
-      feature_map_size = t_layer_shape[1] * t_layer_shape[2]
+      _, height, width, channel = t_layer.get_shape().as_list()
       t_layer_vectorized = tf.reshape(t_layer,
-              shape=[feature_map_size, t_layer_shape[-1]])
-      t_gram_mat = tf.matmul(t_layer_vectorized, t_layer_vectorized, transpose_a=True)
+              shape=[-1, channel])
+      size = height * width * channel
+      t_gram_mat = tf.matmul(t_layer_vectorized, t_layer_vectorized, transpose_a=True) / size
       style_activation = session.run(t_gram_mat)
-      print(layer_name, style_activation.flatten()[: 20])
-      t_style_loss = tf.reduce_mean(tf.square(t_gram_mat - style_activation)) \
-              / (4 * feature_map_size * feature_map_size)
+
+
+def main():
+  graph = tf.Graph()
+  with graph.as_default():
+    session = tf.InteractiveSession(graph=graph)
+    # Load pretrained model
+    X = load_vgg19_v2()
+    # print_graph_node_names(graph)
+
+    content_image = imread(FLAGS.content_image)
+    style_image = imread(FLAGS.style_image)
+    
+    style_layers = [
+      # VGG19 layers
+      'Relu',
+      'Relu_2',
+      'Relu_4',
+      'Relu_8',
+      'Relu_12',
+      # 'BiasAdd', # 'conv1_1/Relu',
+      # 'BiasAdd_2', # 'conv2_1/Relu',
+      # 'BiasAdd_4', # 'conv3_1/Relu',
+      # 'BiasAdd_8', # 'conv4_1/Relu',
+      # 'BiasAdd_12', # 'conv5_1/Relu',
+    ]
+    layer_style_loss_list = []
+    session.run(tf.global_variables_initializer())
+    session.run(X.assign(style_image)) 
+    for layer_name in style_layers:
+      t_layer = T(graph, layer_name)
+      _, height, width, channel = t_layer.get_shape().as_list()
+      t_layer_vectorized = tf.reshape(t_layer,
+              shape=[-1, channel])
+      size = height * width * channel
+      t_gram_mat = tf.matmul(
+        tf.transpose(t_layer_vectorized),
+        t_layer_vectorized,
+      ) / size
+      style_activation = t_gram_mat.eval()
+      # print(layer_name, style_activation.flatten()[: 20])
+      t_style_loss = 2 * tf.nn.l2_loss(t_gram_mat - style_activation) / style_activation.size
       layer_style_loss_list.append(t_style_loss)
      
     content_layers = [
       # VGG19
-      'conv4_2/Relu',
+      # 'MaxPool',
+      # 'pool1',
+      'Relu_9',
     ]
     for layer_name in content_layers:
       print('=================Running layer', layer_name, 'as content layer')
       t_layer = T(graph, layer_name)
       session.run(X.assign(content_image))
       content_activation = session.run(t_layer)
-      t_content_loss = tf.reduce_sum(tf.square(t_layer - content_activation))
+      t_content_loss = 2 * tf.nn.l2_loss(t_layer - content_activation) / content_activation.size
 
       for i in [4]: #reversed(range(5)): # reversed([1, 4, 7, 10, 13]):
         print('Running', style_layers[: i + 1], 'as style layers')
 
         t_total_loss = FLAGS.alpha * t_content_loss
         for t_style_loss in layer_style_loss_list[: i + 1]:
-            t_total_loss += (1 - FLAGS.alpha) * t_style_loss / (i + 1)
-  
+            t_total_loss += (1 - FLAGS.alpha) * t_style_loss
         grad_op = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate) \
                           .minimize(t_total_loss, var_list=[X])
         # Initialize variables needed by Adam
@@ -119,14 +171,22 @@ def main():
           return path
 
         saver = tf.train.Saver()
+        print('initial path: ', FLAGS.initial)
         if FLAGS.resume_iter > 0:
           saver.restore(
             session,
             get_model_save_path(FLAGS.resume_iter))
         else:
-          initial = np.random.uniform(size=(1, FLAGS.img_height, FLAGS.img_width, 3))
+          if FLAGS.initial:
+            initial = imread(FLAGS.initial)
+          else:
+            initial = np.random.normal(size=(1, FLAGS.img_height, FLAGS.img_width, 3), \
+                  scale=np.std(content_image) * 0.1) * 0.256
+          # initial = np.random.uniform(size=(1, FLAGS.img_height, FLAGS.img_width, 3))
           session.run(X.assign(initial))
-        print(session.run(X).flatten()[: 20])
+          print(X.eval().flatten()[: 20])
+
+        print('Loss before starting: ', t_total_loss.eval())
           
         def check_point(num_iter):
           img = session.run(X).squeeze()
@@ -165,6 +225,12 @@ if __name__ == '__main__':
       type=str,
       default='style.jpg',
       help='Path for style image',
+  )
+  parser.add_argument(
+      '--initial',
+      type=str,
+      default='',
+      help='Initiation for optimization',
   )
   parser.add_argument(
       '--learning_rate',
@@ -216,3 +282,4 @@ if __name__ == '__main__':
   )
   FLAGS, _ = parser.parse_known_args()
   main()
+  # get_activation()
